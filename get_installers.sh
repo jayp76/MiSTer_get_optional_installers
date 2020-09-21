8006a9bb9d0a993337b2200d7a54aab88cc63cd6
jayp76 <jayp76@users.noreply.github.com>
2020-09-21 16:13:55 +0200

    Update get_installers.sh

--- get_installers.sh
+++ get_installers.sh
@@ -106,9 +106,8 @@ chmod +x *
 
 echo "Getting Wiki as HTML"
 #get_Wiki MiSTEr_Wiki.html https://raw.githubusercontent.com/jayp76/MiSTer_get_optional_installers/master/MiSTer_FAQ/MiSTer_Wiki_2020-08-31.html
-get_Wiki MiSTEr_Wiki.html https://raw.githubusercontent.com/jayp76/MiSTer_get_optional_installers/master/MiSTer_FAQ/MiSTer_Wiki_2020-09-21.html.xz
-rm -f *.html
-xz -d -v MiSTer_Wiki_2020-09-21.html.xz
+get_Wiki MiSTEr_Wiki.html https://raw.githubusercontent.com/jayp76/MiSTer_get_optional_installers/master/MiSTer_FAQ/MiSTer_Wiki.html.xz
+/bin/xz -d -v MiSTer_Wiki.xz
 
 echo "-=MiSTer Computer Cheat Sheet FAQ by Owlnonymous=-"
 curl -ksLf https://pastebin.com/raw/pM1XMe5E > /media/fat/#help/cheatsheet_$(date +"%Y_%m_%d").txt
