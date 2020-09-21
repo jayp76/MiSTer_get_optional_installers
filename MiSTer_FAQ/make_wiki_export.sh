59a0c0c7812554cdf159d07435988f63d5ef51f6
jayp76 <jayp76@users.noreply.github.com>
2020-09-21 16:10:06 +0200

    Update make_wiki_export.sh

--- MiSTer_FAQ/make_wiki_export.sh
+++ MiSTer_FAQ/make_wiki_export.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 # https://github.com/yakivmospan/github-wikito-converter
 
-# Version 1.0 - 2020-08-31 - First commit
-
 mkdir wiki_temp
 cd ./wiki_temp
+rm -rf *
 
 wget https://raw.githubusercontent.com/jayp76/MiSTer_get_optional_installers/master/MiSTer_FAQ/misterkun_logo.png
 
 git clone https://github.com/MiSTer-devel/Main_MiSTer.wiki.git
 
-gwtc -v -t "MiSTer Wiki" --logo-img misterkun_logo.png -n MiSTer_Wiki_"`date +%F`" ./Main_MiSTer.wiki
+gwtc -v -t "MiSTer Wiki" --logo-img misterkun_logo.png -n MiSTer_Wiki ./Main_MiSTer.wiki
 
-gwtc -v -f pdf -t "MiSTer Wiki" --logo-img misterkun_logo.png -n MiSTer_Wiki_"`date +%F`" ./Main_MiSTer.wiki
+# gwtc -v -f pdf -t "MiSTer Wiki" --logo-img misterkun_logo.png -n MiSTer_Wiki_"`date +%F`" ./Main_MiSTer.wiki
+
+xz -z -v *.html
+
+mv MiSTer_Wiki.html.xz MiSTer_Wiki_"`date +%F`".html.xz
