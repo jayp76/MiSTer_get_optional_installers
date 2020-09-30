172f02d8a89acff8f3d2f829c0a51669fd803b4b
jayp76 <jayp76@users.noreply.github.com>
2020-09-30 15:38:53 +0200

    Update get_cannonball.sh

--- misc/get_cannonball.sh
+++ misc/get_cannonball.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 [[ -d /media/fat/Cannonball ]] || mkdir -p /media/fat/Cannonball ; cd /media/fat/Cannonball
 echo "Getting Cannonball"
-wget --no-check-certificate "https://docs.google.com/uc?export=download&id=10AuxYS48DkEy2xkqTBaIgMgcBw7cMW9T" -O cannonball.7z
+curl -ksJLO --insecure "https://docs.google.com/uc?export=download&id=10AuxYS48DkEy2xkqTBaIgMgcBw7cMW9T"
 curl -LJO --insecure https://raw.githubusercontent.com/develar/7zip-bin//master/linux/arm/7za
 cd /media/fat/Cannonball
 chmod +x 7za
