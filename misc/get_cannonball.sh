a8558d92fbd88892c0b2b1dc9fee5a6c468f7b0c
jayp76 <jayp76@users.noreply.github.com>
2020-09-30 15:22:14 +0200

    Update get_cannonball.sh

--- misc/get_cannonball.sh
+++ misc/get_cannonball.sh
@@ -4,6 +4,6 @@ echo "Getting Cannonball"
 wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=10AuxYS48DkEy2xkqTBaIgMgcBw7cMW9T' -O cannonball.7z
 curl -LJO --insecure https://raw.githubusercontent.com/develar/7zip-bin//master/linux/arm/7za
 chmod +x 7za
-7za x cannonball.7z
-cp Cannonball-OutRun.sh /media/fat/Scripts
-mv Cannonball-OutRun.sh /medai/fat/Scripts/Games
+./7za x ./cannonball.7z
+cp ./Cannonball-OutRun.sh /media/fat/Scripts
+mv ./Cannonball-OutRun.sh /medai/fat/Scripts/Games
