bb2393dfe6c0f4dd89afa1916b05227724f6e44c
jayp76 <jayp76@users.noreply.github.com>
2020-09-30 14:51:19 +0200

    Update get_cannonball.sh

--- misc/get_cannonball.sh
+++ misc/get_cannonball.sh
@@ -2,7 +2,7 @@
 [[ -d /media/fat/Cannonball ]] || mkdir -p /media/fat/Cannonball ; cd /media/fat/Cannonball
 echo "Getting Cannonball"
 wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=10AuxYS48DkEy2xkqTBaIgMgcBw7cMW9T' -O cannonball.7z
-wget --no-check-certificate https://https//raw.githubusercontent.com/develar/7zip-bin//master/linux/arm/7za
+wget --no-check-certificate https://https//raw.githubusercontent.com/develar/7zip-bin//master/linux/arm/7za -O 7za
 chmod +x 7za
 7za x cannonball.7z
 mv Cannonball-OutRun.sh /media/fat/Scripts
