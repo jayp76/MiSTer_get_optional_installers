ce2897c5f8ac3930abcfac122685d814f2b4085d
jayp76 <jayp76@users.noreply.github.com>
2020-09-30 15:44:26 +0200

    Update get_cannonball.sh

--- misc/get_cannonball.sh
+++ misc/get_cannonball.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-[[ -d /media/fat/Cannonball ]] || mkdir -p /media/fat/Cannonball ; cd /media/fat/Cannonball
+cd /media/fat
 echo "Getting Cannonball"
 curl -ksJLO --insecure "https://docs.google.com/uc?export=download&id=10AuxYS48DkEy2xkqTBaIgMgcBw7cMW9T"
 curl -LJO --insecure https://raw.githubusercontent.com/develar/7zip-bin//master/linux/arm/7za
-cd /media/fat/Cannonball
 chmod +x 7za
 ./7za x ./cannonball.7z
 cp ./Cannonball-OutRun.sh /media/fat/Scripts
 mv ./Cannonball-OutRun.sh /media/fat/Scripts/Games
+rm /media/fat/cannonball.7z
