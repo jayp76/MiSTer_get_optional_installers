a4007465db2716d699cb81be73f497ea1729df4d
jayp76 <jayp76@users.noreply.github.com>
2020-09-30 15:11:36 +0200

    Update get_cannonball.sh

--- misc/get_cannonball.sh
+++ misc/get_cannonball.sh
@@ -5,5 +5,5 @@ wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=10Aux
 wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/develar/7zip-bin//master/linux/arm/7za -O 7za
 chmod +x 7za
 7za x cannonball.7z
-mv Cannonball-OutRun.sh /media/fat/Scripts
+cp Cannonball-OutRun.sh /media/fat/Scripts
 mv Cannonball-OutRun.sh /medai/fat/Scripts/Games
