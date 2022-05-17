1fb43440fc021b24fb49d78aba7be78c35881dd6
jayp76 <jayp76@users.noreply.github.com>
2022-05-17 11:02:09 +0200

    bsdtar missing in MiSTer Kernel 5.x release
    substitute with busybox unzip

--- misc/install_devilutionx.sh
+++ misc/install_devilutionx.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-wget -qO- https://github.com/misterkun-io/MiSTer_DeViL/raw/master/releases/Arm-DevilutionX_0.5.0.zip | bsdtar -xvf- -C /media/fat/
+wget -qO- https://github.com/misterkun-io/MiSTer_DeViL/raw/master/releases/Arm-DevilutionX_0.5.0.zip | busybox unzip - -d /media/fat
