8b28720bee2587c0cc0d2dfbff2f4a10d4afeac3
jayp76 <jayp76@users.noreply.github.com>
2023-01-03 15:38:33 +0100

    update to "new" wiki git address

--- MiSTer_FAQ/make_wiki_export.sh
+++ MiSTer_FAQ/make_wiki_export.sh
@@ -7,15 +7,20 @@ rm -rf *
 
 wget https://raw.githubusercontent.com/jayp76/MiSTer_get_optional_installers/master/MiSTer_FAQ/misterkun_logo.png
 
-git clone https://github.com/MiSTer-devel/Main_MiSTer.wiki.git
+#git clone https://github.com/MiSTer-devel/Main_MiSTer.wiki.git
+git clone https://github.com/MiSTer-devel/Wiki_MiSTer.wiki.git
 # Workaround
-touch ./Main_MiSTer.wiki/Arcade-Cores-Top
-touch ./Main_MiSTer.wiki/Arcade-Cores-Bottom
+#touch ./Wiki_MiSTer.wiki/Arcade-Cores-Top
+#touch ./Wiki_MiSTer.wiki/Arcade-Cores-Bottom
 
-gwtc -v -t "MiSTer Wiki" --logo-img misterkun_logo.png -n MiSTer_Wiki ./Main_MiSTer.wiki
 
-# gwtc -v -f pdf -t "MiSTer Wiki" --logo-img misterkun_logo.png --pdf-page-count -n MiSTer_Wiki_"`date +%F`" ./Main_MiSTer.wiki
+#gwtc -v -t "MiSTer Wiki" --logo-img misterkun_logo.png -n MiSTer_Wiki ./Main_MiSTer.wiki
+gwtc -v -t "MiSTer Wiki" --logo-img misterkun_logo.png -n MiSTer_Wiki ./Wiki_MiSTer.wiki
+#lwc -v -t "MiSTer Wiki" --logo-img misterkun_logo.png -n MiSTer_Wiki ./Main_MiSTer.wiki
 
-xz -z -v *.html
+#gwtc -v -f pdf -t "MiSTer Wiki" --logo-img misterkun_logo.png --pdf-page-count -n MiSTer_Wiki_"`date +%F`" ./Main_MiSTer.wiki
+gwtc -v -f pdf -t "MiSTer Wiki" --logo-img misterkun_logo.png --pdf-page-count -n MiSTer_Wiki_"`date +%F`" ./Wiki_MiSTer.wiki
+
+xz -z -v -e *.html
 
 #mv MiSTer_Wiki.html.xz MiSTer_Wiki_"`date +%F`".html.xz
