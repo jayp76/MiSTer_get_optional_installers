e7d34ede74a9d932dd1eaa370da865ad8dc05d95
jayp76 <jayp76@users.noreply.github.com>
2023-08-08 17:25:52 +0200

    rename script

--- misc/get_cave.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-wget -qO- https://archive.org/download/cave-story-mister-fpga/Cave_Story_MiSTer.zip | busybox unzip - -d /media/fat
---
+++ misc/get_cavestory.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+wget -qO- https://archive.org/download/cave-story-mister-fpga/Cave_Story_MiSTer.zip | busybox unzip - -d /media/fat
