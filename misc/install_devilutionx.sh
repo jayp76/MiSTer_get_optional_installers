d436af3358aab74bb25cc957814f7af5111f633c
jayp76 <jayp76@users.noreply.github.com>
2023-08-16 11:53:23 +0200

    Update install_devilutionx.sh

--- misc/install_devilutionx.sh
+++ misc/install_devilutionx.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+#Copy DIABDAT.MPQ from the original Diablo game disc or the GOG version to the Diablo folder on your SD Card.
 
 wget -qO- https://github.com/misterkun-io/MiSTer_DeViL/raw/master/releases/Arm-DevilutionX_0.5.0.zip | busybox unzip - -d /media/fat
